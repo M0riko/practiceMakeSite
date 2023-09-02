76c82796aab456c2c25e4638ff3b5a6a0f94d437
Andrey <adidenko753@gmail.com>
2023-09-03 01:48:22 +0300

    fix: ref code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('nav');
-    const close = document.querySelector('.close');
-    burger.addEventListener('click', () => {
-        nav.classList.add('active');
-        close.classList.remove('none')
+    const body = document.body;
+    const container = body.querySelector('.container');
+    const burger = container.querySelector('.burger');
+    const nav = container.querySelector('nav');
+    const close = container.querySelector('.close');
+    const burgerLine = container.querySelector('.burger_line');
+    container.addEventListener('click', (e) =>  {
+        if(e.target === burger || e.target === burgerLine) {
+            nav.classList.add('active');
+            close.classList.remove('none')
+            body.style.overflow = 'hidden';
+        }
+
+        if(e.target === close) {
+            nav.classList.remove('active');
+            close.classList.add('none')
+            body.style.overflow = 'visible';
+        }
+        if (!nav.contains(e.target) && !(e.target.classList.contains('burger') || e.target.classList.contains('burger_line'))) {
+            nav.classList.remove('active');
+            close.classList.add('none')
+            body.style.overflow = 'visible';
+        }
     })
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-        close.classList.add('none')
-    })
-});
+})
+
+
+
